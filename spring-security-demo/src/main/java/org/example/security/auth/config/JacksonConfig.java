ee45b2931165e8b9d2bd01aa93cbbc7f3cfb89a8
rookie.zhang <1977256156@qq.com>
2020-07-20 00:31:04 +0800

    Tab->spaces

--- spring-security-demo/src/main/java/org/example/security/auth/config/JacksonConfig.java
+++ spring-security-demo/src/main/java/org/example/security/auth/config/JacksonConfig.java
@@ -42,45 +42,45 @@ import java.util.TimeZone;
 @Configuration
 public class JacksonConfig {
 
-	@Bean
-	public Jackson2ObjectMapperBuilderCustomizer customizer() {
-		return builder -> {
-			builder.locale(Locale.CHINA);
-			builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
-			builder.simpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> {
+            builder.locale(Locale.CHINA);
+            builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
+            builder.simpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-			JavaTimeModule javaTimeModule = new JavaTimeModule();
-			javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-			javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-			javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-			javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-			javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-			javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            JavaTimeModule javaTimeModule = new JavaTimeModule();
+            javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
+            javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
 
-			builder.modules(javaTimeModule);
-		};
-	}
+            builder.modules(javaTimeModule);
+        };
+    }
 
 
-	@Bean
-	@Primary
-	@ConditionalOnMissingBean(ObjectMapper.class)
-	public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
-	{
-		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
+    {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
 
-		// 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
-		// Include.Include.ALWAYS 默认
-		// Include.NON_DEFAULT 属性为默认值不序列化
-		// Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的
-		// Include.NON_NULL 属性为NULL 不序列化
-		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		// 允许出现特殊字符和转义符
-		objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
-		// 允许出现单引号
-		objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+        // Include.Include.ALWAYS 默认
+        // Include.NON_DEFAULT 属性为默认值不序列化
+        // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的
+        // Include.NON_NULL 属性为NULL 不序列化
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        // 允许出现特殊字符和转义符
+        objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        // 允许出现单引号
+        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
 		SimpleModule simpleModule = new SimpleModule();
 		/**
@@ -92,8 +92,8 @@ public class JacksonConfig {
 
 		objectMapper.registerModule(simpleModule);
 
-		// 将工具类中的 objectMapper 换为 Spring 中的 objectMapper
-		JacksonUtil.objectMapper = objectMapper;
-		return objectMapper;
-	}
+        // 将工具类中的 objectMapper 换为 Spring 中的 objectMapper
+        JacksonUtil.objectMapper = objectMapper;
+        return objectMapper;
+    }
 }
