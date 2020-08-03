b1eddcc76cb5f4def4ac7164756e9127885dabf2
zhangyanqi <levi.zhang@jcipn.com>
2020-08-03 13:57:16 +0800

    rabbitmq-demo演示类完善

--- spring-rabbitmq-demo/src/main/java/org/example/rabbitmq/auto/component/RabbitJsonConsumer.java
+++ spring-rabbitmq-demo/src/main/java/org/example/rabbitmq/auto/component/RabbitJsonConsumer.java
@@ -5,7 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.rabbitmq.auto.entity.Client;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -22,9 +26,10 @@ public class RabbitJsonConsumer {
     public static final String JSON_QUEUE = "erduo_json";
 
     @RabbitHandler
-    public void onMessage(Client client, Channel channel) throws Exception {
+    public void onMessage(Client client, @Headers Map<String,Object> headers, Channel channel) throws Exception {
         System.out.println("Message content : " + client);
-//        channel.basicAck(messageProperties.getDeliveryTag(),false);
+        System.out.println("Message headers : " + headers);
+        channel.basicAck((Long) headers.get(AmqpHeaders.DELIVERY_TAG),false);
         System.out.println("消息已确认");
     }
 
